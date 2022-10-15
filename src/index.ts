aad5f79af61eac185b87e965002973fe2bd9bbc8
trevor-anderson <T.AndersonProperty@gmail.com>
2022-10-15 11:03:42 -0400

    docs(handler): rm outdated info from handler jsdoc

--- src/index.ts
+++ src/index.ts
@@ -6,36 +6,12 @@ import {
 } from "./pushNotificationHandlers";
 
 /**
- * LAMBDA: fixit-push-notification-service
+ * Fixit Push Notification Service
  *
  * This Lambda function has two invocation sources:
  * 1. fixit-api           async invocation with payload.pushNotifications
  * 2. EventBridge rule    "run-fetchPushReceipts-every-30-minutes"
  *
- * Each push notification must have the following properties:
- *
- * | Property | Description                                                 |
- * | :------- | :---------------------------------------------------------- |
- * | to       | An Expo push notification token.                            |
- * | title    | The title of the push notification.                         |
- * | body     | The body of the push notification message.                  |
- * | Address  | The Address related to the Fixit event.                     |
- * | data     | An object with relevant properties (handled by client app). |
- *
- * Example:
- * ```javascript
- * {
- *  to: "FOO_CONTRACTORS_EXPO_PUSH_TOKEN",
- *  title: "New Work Order Assigned",
- *  body: "Alfred Le Customer-Person has assigned a work order to your company."
- *  Location: {
- *    country: "",
- *    state: "",
- *    "123 Abc Street, Columbus, OH 43210"
- *  },
- *  data: { workOrderID: "workorder_123fooID" }
- * }
- * ```
  */
 export const handler: Handler = async (event: PushServiceEvent) => {
   // TODO After we get 'event' structure printed out, make a better type for it (aws-lambda not helpful here)
